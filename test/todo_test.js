7f55a28d5a60966405cfc6a4ac0889f6ebc0e3fd
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-21 14:22:39 +0800

    Add todo

---
+++ test/todo_test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+describe('todo', function () {
+  it('most things', done)
+  it('.set')
+  it('.extend')
+  it('.sortBy')
+
+  describe('collections', function () {
+    it('.filter / .select')
+    it('.reject')
+    it('.reduce / .reduceRight')
+    it('.every / .all')
+    it('.some / .any')
+    it('.contains / .includes')
+  })
+})
+
+function done () {}
