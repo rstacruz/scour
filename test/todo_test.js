9a878dae7696ad38e1cd34b092ddec3cc4ca4862
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-22 20:58:01 +0800

    Update to do

--- test/todo_test.js
+++ test/todo_test.js
@@ -2,10 +2,13 @@
 
 describe('todo', function () {
   it('most things', done)
-  it('.set')
+  it('.set', done)
+  it('.del', done)
   it('.extend')
+  it('extensions')
   it('.sortBy')
   it('.get() no args', done)
+  it('dot notation', done)
   it('always return scour objects', done)
 
   describe('collections', function () {
