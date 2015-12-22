1131cfe2a0965a746ebd0dacf3b7deaac3ee3979
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-22 18:35:19 +0800

    Improves set implementation

--- test/lib/set_test.js
+++ test/lib/set_test.js
@@ -3,11 +3,43 @@
 const set = require('../../lib/set')
 
 describe('set', function () {
-  var data = { users: { bob: { name: 'robert' } } }
-
   it('works', function () {
+    var data = { users: { bob: { name: 'robert' } } }
     var result = set(data, ['users', 'bob', 'name'], 'john')
 
     expect(result).toEqual({ users: { bob: { name: 'john' } } })
+  })
+
+  it('creates deep structures', function () {
+    var data = { users: 2 }
+    var result = set(data, ['users', 'bob', 'name'], 'john')
+
+    expect(result).toEqual({ users: { bob: { name: 'john' } } })
+  })
+
+  it('handles arrays', function () {
+    var data = { users: [ 'item' ] }
+    var result = set(data, ['users', '0', 'name'], 'john')
+
+    expect(result).toEqual({ users: [
+      { name: 'john' }
+    ]})
+  })
+
+  it('handles arrays', function () {
+    var data = { users: [ 'item' ] }
+    var result = set(data, ['users', '1', 'name'], 'john')
+
+    expect(result).toEqual({ users: [
+      'item',
+      { name: 'john' }
+    ]})
+  })
+
+  it('handles string turning into objects', function () {
+    var data = { users: [ '...' ] }
+    var result = set(data, [ 'users', 0, 'name' ], 'john')
+
+    expect(result).toEqual({ users: [ { name: 'john' } ] })
   })
 })
