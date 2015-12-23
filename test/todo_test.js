4ec2b491c20c287ca521e347ad2dd088f4d7640d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-24 02:26:41 +0800

    Update todo

--- test/todo_test.js
+++ test/todo_test.js
@@ -1,24 +1,33 @@
 'use strict'
 
 describe('todo', function () {
-  it('most things', done)
-  it('.set', done)
-  it('.del', done)
-  it('.extend', done)
-  it('extensions', done)
-  it('.sortBy')
-  it('.get() no args', done)
-  it('dot notation', done)
-  it('always return scour objects', done)
+  describe('v0.3', function () {
+    it('most things', done)
+    it('.set', done)
+    it('.del', done)
+    it('.extend', done)
+    it('extensions', done)
+    it('.sortBy', done)
+    it('.get() no args', done)
+    it('dot notation', done)
+    it('always return scour objects', done)
+  })
+
+  describe('optimizations', function () {
+    it('use native .map and .forEach')
+  })
 
   describe('array', function () {
     it('.gather')
     it('.slice')
     it('.initial')
+    it('.first', done)
+    it('.last', done)
   })
 
   describe('collections', function () {
-    it('.reject')
+    it('.reject', done)
+    it('.pluck')
     it('.reduce / .reduceRight')
     it('.every / .all')
     it('.some / .any')
