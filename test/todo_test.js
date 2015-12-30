560f05ea10fad587be5b31d3f07131c611d9e05a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-30 12:28:49 +0800

    Integrate istanbul

--- test/todo_test.js
+++ test/todo_test.js
@@ -1,42 +1,44 @@
 'use strict'
 
-describe('todo', function () {
-  describe('v0.3', function () {
-    it('most things', done)
-    it('.set', done)
-    it('.del', done)
-    it('.extend', done)
-    it('extensions', done)
-    it('.sortBy', done)
-    it('.get() no args', done)
-    it('dot notation', done)
-    it('always return scour objects', done)
-  })
+const test = require('tape')
 
-  describe('optimizations', function () {
-    it('use native .forEach', done)
-    it('optimize .sortBy(string)', done)
-  })
-
-  describe('array', function () {
-    it('.at', done)
-    it('.go and .at returning objects, ALWAYS', done)
-    it('.getAt', done)
-    it('.gather')
-    it('.slice')
-    it('.initial')
-    it('.first', done)
-    it('.last', done)
-  })
-
-  describe('collections', function () {
-    it('.reject', done)
-    it('.pluck')
-    it('.reduce / .reduceRight')
-    it('.every / .all')
-    it('.some / .any')
-    it('.contains / .includes')
-  })
+test('todo: v0.3', (t) => {
+  t.ok(1, 'most things')
+  t.ok(1, '.set')
+  t.ok(1, '.del')
+  t.ok(1, '.extend')
+  t.ok(1, 'extensions')
+  t.ok(1, '.sortBy')
+  t.ok(1, '.get() no args')
+  t.ok(1, 'dot notation')
+  t.ok(1, 'always return scour objects')
+  t.end()
 })
 
-function done () {}
+test('todo: optimizations', (t) => {
+  t.ok(1, 'use native .forEach')
+  t.ok(1, 'optimize .sortBy(string)')
+  t.end()
+})
+
+test('todo: array', (t) => {
+  t.ok(1, '.at')
+  t.ok(1, '.go and .at returning objects, ALWAYS')
+  t.ok(1, '.getAt')
+  t.ok(1, '.gather')
+  t.skip('.slice')
+  t.skip('.initial')
+  t.ok(1, '.first')
+  t.ok(1, '.last')
+  t.end()
+})
+
+test('todo: collections', (t) => {
+  t.ok(1, '.reject')
+  t.skip('.pluck')
+  t.skip('.reduce / .reduceRight')
+  t.skip('.every / .all')
+  t.skip('.some / .any')
+  t.skip('.contains / .includes')
+  t.end()
+})
