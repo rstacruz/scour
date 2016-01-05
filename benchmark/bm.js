2ffcbd5289c254aefd05b2628617bec3f64f1dad
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-06 07:04:56 +0800

    Remove benchmark.js

--- benchmark/bm.js
+++ benchmark/bm.js
@@ -1,26 +1,47 @@
-module.exports = function bm (suitename, options) {
-  var Benchmark = require('benchmark')
-  var suite = new Benchmark.Suite(suitename)
+var assign = require('object-assign')
 
-  for (var key in options) {
-    if (!options.hasOwnProperty(key)) continue
-    suite = suite.add(key, options[key])
+var defaults = {
+  onStart: function (suitename) { console.log(suitename) },
+  onStep: function (key) { },
+  onStepEnd: function (key, result) { console.log('    x ' + result.rate + ' op/sec - ' + key) },
+  onError: function (err) { console.error(err.stack) }
+}
+
+function bm (suitename, steps, options) {
+  options = assign({}, defaults, options)
+  if (!options) options = {}
+  options.onStart(suitename, steps, options)
+  for (var key in steps) {
+    if (!steps.hasOwnProperty(key)) continue
+    try {
+      options.onStep(key)
+      var result = bm.runBenchmark(steps[key])
+      options.onStepEnd(key, result)
+    } catch (err) {
+      options.onError(err)
+    }
+  }
+}
+
+bm.timeout = 3
+
+bm.runBenchmark = function runBenchmark (fn) {
+  var start = +new Date()
+  var timeout = bm.timeout
+  var elapsed
+  var iterations = 0
+
+  while (true) {
+    fn()
+    iterations += 1
+    elapsed = (+new Date() - start) / 1000
+    if (elapsed > timeout) break
   }
 
-  suite.on('start', function (event) {
-    console.log(suitename)
-  })
+  var time = elapsed
+  var rate = iterations / time
 
-  suite.on('cycle', function (event) {
-    console.log('  ' + String(event.target))
-  })
-
-  suite.on('error', function (e) {
-    console.log(e.target.error.stack)
-  })
-
-  suite.on('complete', function () {
-  })
-
-  suite.run({ async: true, maxTime: 2 })
+  return { time: time, rate: Math.round(rate), iterations: iterations }
 }
+
+module.exports = bm
